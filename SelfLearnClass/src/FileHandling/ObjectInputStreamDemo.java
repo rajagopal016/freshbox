cab7627d970c17fae36c793e4b2f5c6856c1cdd9
rajagopal <rajagopal016@gmail.com>
2021-06-27 20:48:27 +0530

    File IO 3

---
+++ SelfLearnClass/src/FileHandling/ObjectInputStreamDemo.java
@@ -0,0 +1,32 @@
+package FileHandling;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ObjectInputStreamDemo {
+
+	public static void main(String[] args) {
+		ObjectInputStream stream = null;
+		
+		try {
+			stream = new ObjectInputStream(new FileInputStream("objIO"));
+			System.out.println(stream.readObject());
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+
+	}
+
+}
